b51f3a804d2fb2d153f0579f15d53dd09f97eda3
Dyego Barros <dyegobarros@iMac-de-Dyego.local>
2025-10-07 15:56:32 -0300

    Primeiro commit do projeto

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI(debug=True, title="Teste FastAPI")
+
+@app.get("/teste")
+def index():
+    return {"Message": "Olá, esse é um teste com FastAPI!"}
+
+@app.get("/")
+def home():
+    return {"status": "ok", "message": "API FastAPI rodando com sucesso!"}
+
+
+if __name__ == "__main__":
+    import uvicorn 
+    uvicorn.run("main:app", host="0.0.0.0", port=6000, reload=True)
